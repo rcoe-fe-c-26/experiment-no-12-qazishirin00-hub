f59a06c4d52a90ed5e91234b0f3244640aecea8e
qazishirin00-hub <qazishirin00@eng.rizvi.edu.in>
2026-02-14 13:01:09 +0530

    Update main.pyexp12

--- main.py
+++ main.py
@@ -1,8 +1,27 @@
 # AIM: Develop a Python program that reads a text file and 
 # prints words of specified lengths (e.g., three, four, 
 # five, etc.) found within the file.
-# Coder:
-# Date:
+# Coder: Shirin 
+# Date:14-02-2026 
 
 print("--- Extracting Words from Text File ---\n")
+
+with open("story.txt", "r") as file:
+    content = file.read()
+
+words = content.split()
+
+length = int(input("Enter Length of words: "))
+
+unique_words = set()
+
+for word in words:
+    word = word.lower()
+    if len(word) == length:
+        unique_words.add(word)
+
+result = sorted(unique_words)
+
+print("Words with length", length, "are:", result)
+
 
